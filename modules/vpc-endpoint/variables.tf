d0baec3b1b09aead7afd9e5a4377def711677429
Chris Lamb <37453482+lambchr@users.noreply.github.com>
2025-02-27 15:14:43 +0000

    fix: remove bump (#9)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/vpc-endpoint/variables.tf
+++ modules/vpc-endpoint/variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_id" {
-  description = "The ID of the VPC in which the endpoint will be used bump"
+  description = "The ID of the VPC in which the endpoint will be used"
   type        = string
   default     = null
 }
